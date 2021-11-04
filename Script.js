f7a0e1d2162871f201fe939bd3e35b82db4a4934
JoseT360 <elitexcrude12345@gmail.com>
2021-11-04 14:45:22 -0400

    Hello

--- Script.js
+++ Script.js
@@ -3,33 +3,45 @@ var p2Score = 0;
 var tie = 0;
 var turn = 1;
 
-function checkWin(){
-    if ($(".row-1 .box.x").length === 3){
-        console.log("working")
-    }
-    if ($(".row-2 .box.x").length === 3){
-        console.log("working2")
-    }
-
-    if ($(".row-3 .box.x").length === 3){
-        console.log("working3")
-    }
-    if ($(".col-1 .box.x").length === 3){
-        console.log("working")
-    }
-    if ($(".col-2 .box.x").length === 3){
-        console.log("working")
-    }
-    if ($(".col-3 .box.x").length === 3){
-        console.log("working")
-    }
-    if ($("#1").hasClass("x")
+function checkwinner(){
+    if ($(".row-1 .box.x").length === 3 ||
+    $(".row-2 .box.x").length === 3 ||
+    $(".row-3 .box.x").length === 3 ||
+    $(".col-1 .box.x").length === 3 ||
+    $(".col-2 .box.x").length === 3 ||
+    $(".col-3 .box.x").length === 3 ||
+    $("#1").hasClass("x")
     && $("#5").hasClass("x")
-    && $("#9").hasClass("x"))
+    && $("#9").hasClass("x") ||
+    $("#3").hasClass("x")
+    &&$("#5").hasClass("x")
+    &&$("#7").hasClass("x"))
     {
-        console.log("working")
+        console.log("P1 Win");
+        p1score++;
+        $("#p1score").text(p1score)
+    }}
+
+
+    function checkwinnero(){
+        if ($(".row-1 .box.o").length === 3 ||
+        $(".row-2 .box.o").length === 3 ||
+        $(".row-3 .box.o").length === 3 ||
+        $(".col-1 .box.o").length === 3 ||
+        $(".col-2 .box.o").length === 3 ||
+        $(".col-3 .box.o").length === 3 ||
+        $("#1").hasClass("o")
+        && $("#5").hasClass("o")
+        && $("#9").hasClass("o") ||
+        $("#3").hasClass("o")
+        &&$("#5").hasClass("o")
+        &&$("#7").hasClass("o"))
+        {
+            console.log("P2 Win");
+            p2score++;
+            $("#p2score").text(p2score)
+        }
     }
-}
 
 
 
@@ -40,13 +52,13 @@ $(".box").click(function(){
         $(this).addClass("x")
         turn = 2;
         $("#turn").text(2)
-        checkWin();
+        checkwinner();
     }
-    else
-    {
+    else{
         $(this).text("o")
         $(this).addClass("o")
         turn = 1;
         $("#turn").text(1)
+        checkwinnero();
     }
 })
